6fe1fa7cdf91324280f1b37b1e5c63d677ce88d0
tforbus <tristinforbus@gmail.com>
2013-09-04 23:13:42 -0500

    favicon

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ var app = express();
 app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
-app.use(express.favicon());
+app.use(express.favicon(__dirname + '/public/images/favicon.png')); 
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
